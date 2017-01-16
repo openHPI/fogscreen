203f3c0e4806f08aef02a261dfa4f5225f4e1f99
Jan Renz <jan.renz@hpi.de>
2017-01-16 19:41:33 +0100

    alpha

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home', function(){
+    this.route('start');
+    this.route('openhpi');
+  });
 });
 
 export default Router;
